6a6ae82be22a270742599df81335f1edbe7c88e9
Aeneas Schwaiger <schwaiger@max-eyth-schule.de>
2025-12-01 14:51:03 +0100

    Mutex für Thread-Sicherheit

--- Beispiel_Java_liste/List.h
+++ Beispiel_Java_liste/List.h
@@ -1,108 +1,126 @@
+// Stand: 01.12.2025 - Schwaiger
 #pragma once
 #include <list>
+#include <mutex>
+#include <algorithm>
+
 using namespace std;
 
 // Eine Liste für eine beliebige Klasse T
 template<class T> class List
 {
 private:
-	list <T> * meineListe;
+    list<T>* meineListe;
+    mutable mutex m;      // Mutex für Thread-Sicherheit
+
 public:
-	List()			//Konstruktor mit dynamischer Erzeugung einer Liste vom Typ list 
-	{
-		meineListe = new list<T>;
-	}
-	~List() { 
-		//delete meineListe; // @Aeneas: gelöscht, da sonst eine Liste in der Liste gelöscht wird (siehe Aquarium Lebewesenliste)
-	}
+    List()   //Konstruktor mit dynamischer Erzeugung einer Liste vom Typ list 
+    {
+        meineListe = new list<T>;
+    }
 
-	void add(T);		// hängt das Objekt obj vom Typ T am Ende der Liste an.
+    ~List()
+    {
+        // ggf. nicht löschen, falls Elemente anders verwaltet werden
+        delete meineListe;
+    }
 
-	void add(int index, T obj);  //fügt das Objekt obj vom Typ T an der Position index in die Liste ein.
 
-	bool contains(T obj);    //liefert true, wenn das Objekt obj in der Liste enthalten ist, andernfalls false.
+    void add(T obj);                 // hängt das Objekt am Ende der Liste an.
+    void add(int index, T obj);      // fügt das Objekt an Position index ein.
 
-	T get(int index);       //liefert das Listenelement an der Position index (bei Null beginnend) zurück bzw. null, falls index negativ oder größer
-						    //gleich der Anzahl der momentan enthaltenen Elemente ist.
+    bool contains(T obj);            // true, wenn Objekt enthalten ist.
 
-	T remove(int index);	//entfernt das Listenelement an der Position index (bei Null beginnend).Liefert das entfernte Element zurück bzw.null,
-							//falls index negativ oder größer gleich der Anzahl der momentan enthaltenen Elemente ist.
+    T get(int index);                // liefert Listenelement an Position index.
 
-	bool remove(T obj);	//entfernt das Objekt obj aus der Liste. Falls obj mehrmals in der Liste enthalten ist, wird nur das erste
-							//Vorkommen entfernt.Der Rückgabewert ist true, falls das Objekt gefunden und entfernt wurde,
-							//andernfalls ist er false.
-	int size();				//liefert die Anzahl der Elemente in der Liste zurück.
+    T remove(int index);             // entfernt Element an Position index.
+
+    bool remove(T obj);              // entfernt das erste Vorkommen von obj.
+
+    int size();                      // Anzahl der Elemente in der Liste.
 };
-
-
-
 
 
 template<class T> int List<T>::size()
 {
-	return meineListe->size();
+    lock_guard<mutex> lock(m);
+    return (int)meineListe->size();
 }
 
-
-// Neues Element in die Liste einfügen
+// Neues Element in die Liste einfügen (am Ende)
 template<class T> void List<T>::add(T obj)
 {
-	meineListe->push_back(obj);
+    lock_guard<mutex> lock(m);
+    meineListe->push_back(obj);
 }
 
+// Neues Element an Position index einfügen
 template<class T> void List<T>::add(int index, T obj)
 {
-	if (index < 0 || index >= meineListe->size())
-		return;
-	list<T>::template iterator it = meineListe->begin();
-	advance(it, index);
+    lock_guard<mutex> lock(m);
 
-	meineListe->insert(it, obj);
+    if (index < 0 || index >(int)meineListe->size())
+        return;
+
+    typename list<T>::iterator it = meineListe->begin();
+    advance(it, index);
+    meineListe->insert(it, obj);
 }
 
 template<class T> bool List<T>::contains(T obj)
 {
-	bool found = (std::find(meineListe->begin(), meineListe->end(), obj) != meineListe->end());
-	return found;
+    lock_guard<mutex> lock(m);
+
+    bool found = (std::find(meineListe->begin(), meineListe->end(), obj) != meineListe->end());
+    return found;
 }
 
-template<class T> T  List<T>::get(int index)
+template<class T> T List<T>::get(int index)
 {
-	if (index < 0 || index >= meineListe->size())
-		return 0;
-	int i = 0;
+    lock_guard<mutex> lock(m);
 
-	for (auto cursor : (*meineListe))
-	{
-		if (i == index)
-			return cursor;
-		i++;
-	}
-	return 0;
-}
-template<class T> T  List<T>::remove(int index)
-{
-	if (index < 0 || index >= meineListe->size())
-		return 0;
-	T tmp = this->get(index);
-	list<T>::template iterator it = meineListe->begin();
-	advance(it, index);
-	meineListe->erase(it);
-	return tmp;
+    if (index < 0 || index >= (int)meineListe->size())
+        return 0;
+
+    int i = 0;
+    for (auto& cursor : *meineListe)
+    {
+        if (i == index)
+            return cursor;
+        i++;
+    }
+    return 0;
 }
 
-template<class T> bool  List<T>::remove(T obj)
+template<class T> T List<T>::remove(int index)
 {
-	if (obj == 0)
-		return false;
+    lock_guard<mutex> lock(m);
 
-	for (typename std::list<T>::iterator it = meineListe->begin(); it != meineListe->end(); ++it)
-	{
-		if ((*it) == obj)
-		{
-			meineListe->erase(it);
-			return true;
-		}
-	}
-	return false;
+    if (index < 0 || index >= (int)meineListe->size())
+        return 0;
+
+    typename list<T>::iterator it = meineListe->begin();
+    advance(it, index);
+
+    T tmp = *it;
+    meineListe->erase(it);
+    return tmp;
+}
+
+template<class T> bool List<T>::remove(T obj)
+{
+    lock_guard<mutex> lock(m);
+
+    if (obj == 0)
+        return false;
+
+    for (typename std::list<T>::iterator it = meineListe->begin(); it != meineListe->end(); ++it)
+    {
+        if ((*it) == obj)
+        {
+            meineListe->erase(it);
+            return true;
+        }
+    }
+    return false;
 }
